86e271587afff2c766698e80b068b59dfe76c9b4
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-05-18 15:50:24 +0100

    Todo Schema
    - hello world index page added
    - db.create_all() will create the tables on the database

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask
 
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://kehinde@localhost:5432/example"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://kehinde@localhost:5432/todoapp"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
@@ -16,14 +16,18 @@ class Todo(db.Model):
     __tablename__ = "todos"
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr__(self) -> str:
-        return f"<Todo ID: {self.id}, name: {self.description}>"
+        return f"<Todo ID: {self.id}, description: {self.description} Completed: {self.completed}>"
 
 
-# create tables from Models
-db.create_all()
+db.create_all()  #   create tables for the specified schema(s)
 
+
+@app.route("/")
+def index():
+    return "Hello world"
 
 db.session.add(
     Todo(description="Life is very interesting. I love to make money than I need.")
@@ -33,5 +37,5 @@ db.session.commit()  # changes the state to committed
 
 todos = Todo.query.all()  # changes the state to Flushed
 
-print("A todo item\n", todos[0])
-print("A list of all todos\n", todos)
+if __name__ == "__main__":
+    app.run(debug=True)
