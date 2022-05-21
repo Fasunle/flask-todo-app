e516020011135eb3a59115156749737f026dadcd
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-05-21 03:13:41 +0100

    Update TodoList Schema: TodoList should have name
    A todolist should have a name or category.

--- app.py
+++ app.py
@@ -29,10 +29,11 @@ class TodoList(db.Model):
 
     __tablename__ = "todolists"
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
     todos = db.relationship("Todo", backref="list", lazy=True)
 
     def __repr__(self) -> str:
-        return f"TodoList: [{self.todos}]"
+        return f"TodoList( name: {self.name}, [{self.todos}])"
 
 
 @app.route("/")
