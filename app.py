be8f3bf05108495d3e82d9b986604a852bd625ed
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-05-21 04:57:45 +0100

    list_id should not be nullable
    This is easy to do since we will be migrating the schema with flask-migrate

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
     completed = db.Column(db.Boolean, nullable=False, default=False)
-    list_id = db.Column(db.Integer, db.ForeignKey("todolists.id"), nullable=True)
+    list_id = db.Column(db.Integer, db.ForeignKey("todolists.id"), nullable=False)
 
     def __repr__(self) -> str:
         return f"<Todo ID: {self.id}, description: {self.description} Completed: {self.completed}>"
